1115dce050a24475f50927b0c0c4ad811a42572a
tiouoo <tiouo@qq.com>
2026-08-16 17:18:29 +0800

    refactor(Portal): 移除 MainViewModel 中的问候属性

--- src/Portal/ViewModels/MainViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Portal.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    [ObservableProperty] public partial string Greeting { get; set; } = "Welcome to Avalonia!";
-}
